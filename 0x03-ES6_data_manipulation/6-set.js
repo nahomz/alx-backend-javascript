4572e3ec848e3346c4531628e7869bbe842651a5
nahomz <nahomzelalem11@gmail.com>
2022-06-22 20:16:40 +0000

    data-maipulation intial push

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Nahom Zelalem <https://github.com/nahomz>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
